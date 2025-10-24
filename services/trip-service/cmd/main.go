e5e43008d9b43bc82f16510d8ee0b75edb79cb2a
Nyxoy77 <shivamrai0147@gmail.com>
2025-10-24 09:53:38 +0000

    Refactor main function to implement graceful server shutdown and improve error handling

--- services/trip-service/cmd/main.go
+++ services/trip-service/cmd/main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"context"
 	"log"
+	"os"
+	"os/signal"
 	"ride-sharing/services/trip-service/internal/infrastructure/http"
 	"ride-sharing/services/trip-service/internal/infrastructure/repository"
 	"ride-sharing/services/trip-service/internal/service"
+	"syscall"
+	"time"
+
+	stdhttp "net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -16,15 +23,43 @@ func main() {
 	// 	ID: primitive.NewObjectID(),
 	// }
 
-	router := gin.Default()
+	router := gin.Default() // This is a handler which can be passed to an actual http server
 
 	httpHandler := http.HttpHandler{Service: svc}
 	router.POST("/preview", httpHandler.HandleTripPreview)
 
-	if err := router.Run(); err != nil {
-		log.Println("error running the http server")
-		return
+	server := &stdhttp.Server{
+		Addr:    "8080",
+		Handler: router,
 	}
-	// router.Run()
+
+	serverChannel := make(chan error, 1)
+
+	go func() {
+		serverChannel <- server.ListenAndServe()
+	}()
+
+	sigChannel := make(chan os.Signal, 1)
+
+	signal.Notify(sigChannel, os.Interrupt, syscall.SIGTERM)
+
+	select {
+	case err := <-serverChannel:
+		log.Printf("Error starting the server: %v", err)
+
+	case sig := <-sigChannel:
+		log.Printf("Server gracefully shutting down because of signal %v ", sig)
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		if err := server.Shutdown(ctx); err != nil {
+			log.Printf("Could not stop the server gracefully %v ", err)
+			server.Close()
+		}
+
+	}
+
+	log.Println("gracefully shutting down the server")
 
 }
